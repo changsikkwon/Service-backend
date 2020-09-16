32e18c4c05819dc1b1fc404ed1c9d0d28c29ce4b
chang sik kwon <kcs1987@gamil.com>
2020-09-16 14:15:34 +0900

    초기세팅:db connection 확인

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
+import config
+
 from flask                         import Flask
 from flask_cors                    import CORS
 
-from config                        import database
 from sqlalchemy                    import create_engine
+from model                         import TestDao
+from service                       import TestService
+from controller                    import create_endpoints
 
 def create_app(test_config = None):
     app = Flask(__name__)
+    app.debug = True
     
-    # DB연결
     if test_config is None:
-        app.config.from_pyfile("config.py")
+        app.config.from_pyfile('config.py')
     else:
         app.config.update(test_config)
         
-    database = create_engine(app.comfig['DB_URL'], encoding = 'utf-8', max_overflow = 0)
-    app.database = database
+    # DB 연결
+    database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
 
-    app.debug = True
-
-#     # CORS 설정
+    # CORS 설정
     CORS(app, resources={r'*' : {'origins': '*'}})
+    
+    # Persistance layer
+    test_dao = TestDao(database)
+    
+    # Business layer
+    test_service = TestService(test_dao)
+    
+    # Presentation layer
+    create_endpoints(app, test_service)
 
     return app
