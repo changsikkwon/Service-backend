3b4396e05404a82f3dea20ef95866f30a9d6e9c1
JiwonKo <rhwldnjs1226@gmail.com>
2020-09-23 19:26:10 +0900

    [초기 세팅 수정 및 상품 데이터 전달 API 수정] - session 초기세팅pool size 및 max overflow 추가 - model 에서 dao 로 파일명 수정 - 상품 리스트 API 에서 seller 정보 JOIN 으로 가져와서 표출하도록 변경 - 상품 리스트 API 에서 create_at 컬럼을 기준으로 가장 최근 이력을 가져오도록 변경

--- controller/product_controller.py
+++ controller/product_controller.py
@@ -1,12 +1,26 @@
 from flask import request, jsonify, Blueprint
+# from flask_request_validtor import GET, Param, validate_params
 
 def create_product_endpoints(product_service, Session):
 
-    product_app = Blueprint('product_app', __name__, url_prefix='/products')
+    product_app = Blueprint('product_app', __name__, url_prefix='/api/products')
 
     @product_app.route('/category', methods = ['GET'])
     def product_category():
-        """ 카테고리 정보 전달 API """
+        """ 카테고리 정보 전달 API
+
+        상위 카테고리에 따른 하위 카테고리 리스트를 전달합니다.
+
+        returns :
+            200: 카테고리 리스트
+            500:
+
+        Authors:
+            고지원
+
+        History:
+            2020-09-21 (고지원): 초기 생성
+        """
         try:
             session = Session()
             body = [{
@@ -23,12 +37,31 @@ def create_product_endpoints(product_service, Session):
 
         except Exception as e:
             return jsonify({'message' : f'{e}'}), 500
+
         finally:
             session.close()
 
     @product_app.route('', methods = ['GET'])
+    # @validate_params(
+    #     Param('limit', GET, int),
+    #     Param('offset', GET, int),
+    #     Param('first_category_id', GET, int, required=False),
+    #     Param('second_category_id', GET, int, required=False),
+    # )
     def products():
-        """ 상품 정보 전달 API """
+        """ 상품 정보 전달 API
+        여러 상품 정보가 필요한 페이지에서 쿼리 파라미터로 필터링에 사용될 값을 받아 필터링된 상품의 데이터들을 표출합니다.
+
+        returns :
+            200: 상품 리스트
+            500:
+
+        Authors:
+            고지원
+
+        History:
+            2020-09-21 (고지원): 초기 생성
+        """
         try:
             session = Session()
             # pagination
@@ -54,13 +87,71 @@ def create_product_endpoints(product_service, Session):
                 'sales_amount'   : product.sales_amount,
                 'discount_rate'  : product.discount_rate,
                 'discount_price' : product.discount_price,
-                'seller_name'    : product_service.get_seller_info(product.seller_id, session).korean_name
+                'seller_name'    : product.korean_name
             } for product in product_service.get_products(filter_dict, session)]
-            print(jsonify(body))
             return jsonify(body), 200
 
         except Exception as e:
             return jsonify({'message' : f'{e}'}), 500
+
+        finally:
+            session.close()
+
+    @product_app.route('/product/<int:product_id>', methods=['GET'])
+    def product(product_id):
+        """ 상품 상세 정보 전달 API
+        path parameter 를 받아 한 상품의 상세 데이터를 표출합니다.
+
+        returns :
+            200: 상위 카테고리에 따른 하위 카테고리 리스트
+            500:
+
+        Authors:
+            고지원
+
+        History:
+            2020-09-23 (고지원): 초기 생성
+        """
+        try:
+            session = Session()
+            product = product_service.get_product(product_id, session)
+            seller = product_service.get_seller(product['seller_id'], session)
+
+            body = {
+                'id'             : product['p_id'],
+                'name'           : product['name'],
+                'price'          : product['price'],
+                'discount_rate'  : product['discount_rate'],
+                'discount_price' : product['discount_price'],
+                'image'          : product['images'],
+                'color'          : product['colors'],
+                'size'           : product['sizes'],
+                'sales_amount'   : product['sales_amount'],
+                'seller_name'    : seller.korean_name,
+                'seller_url'     : seller.site_url,
+                'review_rating'  : 'rating',
+                'review_count'   : 'count'
+            }
+            return jsonify(body), 200
+
+        except Exception as e:
+            return jsonify({'message': f'{e}'}), 500
+
+        finally:
+            session.close()
+
+    @product_app.route('/sellers', methods=['GET'])
+    def sellers():
+        try:
+            session = Session()
+            body = [{
+                'count' : seller['count'],
+                'id'    : seller['id'],
+                'name'  : seller['korean_name'],
+                'image' : seller['image_url'],
+                'url'   : seller['site_url']
+            }for seller in product_service.get_sellers('무드글램', session)]
+            return jsonify(body), 200
         finally:
             session.close()
 
